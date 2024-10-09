b75d94fa0aa3f7fb521a2d76638a285c68a2b8f7
Fiidel <ada.helesic@gmail.com>
2024-10-09 15:22:52 +0200

    feat(PacketCapture): added udp payload parsing

--- src/PacketCapture.cpp
+++ src/PacketCapture.cpp
@@ -17,10 +17,41 @@ PacketCapture::PacketCapture()
     signal(SIGINT, InterruptHandler);
 }
 
-void packet_handler(u_char *userArg, const struct pcap_pkthdr *header, const u_char *bytes)
+void packet_handler(u_char *userArg, const struct pcap_pkthdr *header, const u_char *packet)
 {
-    // TODO: packet work
-    std::cout << "Packet in packet handler." << std::endl;
+    // ethernet header is 14B
+    ushort ethHeaderLength = 14;
+    // IPv4 header is variable, IPv6 header is 40B
+    // UDP header is 8B
+    ushort udpHeaderLength = 8;
+    
+    // Determine the IP header length.
+    ushort ipHeaderLength = 0;
+    ushort ipVersion = packet[14] >> 4;
+    // if IPv4
+    if (ipVersion == 4)
+    {
+        // IP header's TIL 4b field contains the length of the IP header as a number of 32b words in the header
+        ipHeaderLength = (ushort) packet[ethHeaderLength] & 0b00001111;
+        ipHeaderLength *= 4;
+    }
+    // if IPv6
+    else if (ipVersion == 6)
+    {
+        ipHeaderLength = 40;
+    }
+    else
+    {
+        std::cerr << "IP version incorrect. Ignoring packet." << std::endl;
+        return;
+    }
+
+    // DNS payload and length
+    const u_char *DnsPayload = &packet[ethHeaderLength + ipHeaderLength + udpHeaderLength];
+    ushort DnsPayloadLength = (ushort) (((packet[ethHeaderLength + ipHeaderLength + 4] << 8)
+        + packet[ethHeaderLength + ipHeaderLength + 5]) 
+        - udpHeaderLength);
+
     return;
 }
 
